b88efb944f3ff32f90f7a215a4deb4d7b1e89373
Antoine <ant.dupre@gmail.com>
2018-09-24 17:44:44 +0200

    #4 Liste les Class possibles

--- assets/js/interface.js
+++ assets/js/interface.js
@@ -4,7 +4,8 @@
  
         var specSearch = {} ;
 		var defaults = {
-			pathSpecSearch: 'config/spec-search.json'
+			pathSpecSearch: 'config/spec-search.json',
+			language: 'fr'
 		};
 		
 		var settings = $.extend( {}, defaults, options );
@@ -14,13 +15,10 @@
 		this.each(function() {
             var thisForm = $(this) ;
 			$.when( loadSpecSearch() ).done(function() {
-				var items = initForm() ;
 				
+					initForm(thisForm) ;
 				
-				$( "<ul/>", {
-				"class": "my-new-list",
-				html: items.join( "" )
-			  }).appendTo( thisForm );
+
 				 });
 				
         });
@@ -35,30 +33,152 @@
 		function initForm(thisForm_) {
 			console.log(specSearch) ;
 			
+			var list = getClassListSelectFor(null) ;
+			$(thisForm_).append(list) ;
+			
+		}
+		/*  Find Class by ID
+			@Id of Class
+			return object of @type Class in specSearch 
+		*/
+		function getClassById(ClassId) {
+			var classObject = null ;
+			$.each( specSearch['@graph'], function( key, val ) {
+				if ( val['@type'] == 'Class') {
+					if ( val['@id'] == ClassId) {
+							classObject = val ;
+					}
+				}
+			}) ;
+			return classObject ;
+		}
+		function getClassLabel(ClassId) {
+			var classLabel = null ;
+			var classObject = getClassById(ClassId) ;
+			if (classObject !== null) {
+				$.each( classObject['label'], function( key, val ) {
+					if (val['@language'] == settings.language ) {
+						classLabel = val['@value'] ;
+					}
+				});
+			}
+			return classLabel ;
+		}
+		
+		
+		function getClassListSelectFor(classId) {
+			var list = [] ;
+			var items = getAllClassFor(classId) ;
+			$.each( items, function( key, val ) {
+				var label = getClassLabel(val['@id']) ;
+				list.push( '<li data-id="'+val['@id']+'">'+ label + ' => '+val['@id']+'</li>' );
+
+			}) ;
+			var html_list = $( "<ul/>", {
+				"class": "my-new-list",
+				html: list.join( "" )
+			  });
+			return html_list ;
+		}
+		
+		
+		/*  Find if Class is in objectProperty domain list
+			@Id of objectProperty where search
+			@Id of Class we will retrive
+			return true if  we find the class or false
+		*/
+		function classIsInDomain(ObjectPropertyId, ClassId) {
+			var classIsDomain = false ;
+			$.each( specSearch['@graph'], function( key, val ) {
+				if ( ( val['@type'] == 'ObjectProperty') &&  (val['@id'] == ObjectPropertyId) ){
+					if ($.type(val['domain']) === "object") {
+						$.each( val['domain']['unionOf']['@list'], function( domkey, domval ) {
+							if (domval['@id'] == ClassId ) {
+								classIsDomain = true
+							}
+						}) ;
+					} else {
+						if (val['domain'] == ClassId ) {
+							classIsDomain = true ;
+						}
+					}
+				}
+			});
+			return classIsDomain;
+		}
+		/* List of possible Class relative to a Class
+			@Id of Class or null if is the first list selection
+			return array of @type Class in specSearch 
+		*/
+		function getAllClassFor(ClassID) {
+			var items = [];
+			$searchKey = 'range' ;
+			if (ClassID === null) {
+				$searchKey = 'domain' ;
+			}
+			$.each( specSearch['@graph'], function( key, val ) {
+				if ( val['@type'] == 'ObjectProperty') {
+					if ($.type(val[$searchKey]) === "object") {
+						$.each( val[$searchKey]['unionOf']['@list'], function( domkey, domval ) {
+							if (ClassID === null) {
+								var item = getClassById(domval['@id']) ;
+								items = pushIfNotInArray(item, items);
+							} else {
+								if (classIsInDomain(val['@id'], ClassId)) {
+									var item = getClassById(domval['@id']) ;
+									items = pushIfNotInArray(item, items);
+								}
+							}
+						}) ;
+					} else {						
+						if (ClassID === null) {
+								var item = getClassById(val[$searchKey]) ;
+								items = pushIfNotInArray(item, items);
+						} else {
+							if (classIsInDomain(val[$searchKey], ClassId)) {
+								var item = getClassById(val[$searchKey]) ;
+								items = pushIfNotInArray(item, items);
+							}
+						}
+					}
+				}
+			});
+			return items ;
+		}
+		/* List of possible ObjectProperty relative to a Class
+			@Id of Class
+			return array of @type ObjectProperty in specSearch 
+		*/
+		function getAllObjectPropertyFor(ClassId) {
 			var items = [];
 			$.each( specSearch['@graph'], function( key, val ) {
 				if ( val['@type'] == 'ObjectProperty') {
-					console.log(val) ;
 					if ($.type(val['domain']) === "object") {
 						$.each( val['domain']['unionOf']['@list'], function( domkey, domval ) {
-
-							items.push( "<li id='" + domkey + "'>" + key+ "-"+ domkey + " - " + domval['@id'] + "</li>" );
+							if (domval['@id'] == ClassId ) {
+								items = pushIfNotInArray(val, items);
+							}
 						}) ;
-						
 					} else {
-						console.log(val)
-						items.push( "<li id='" + key + "-1'>" + key + " - " + val.domain + "</li>" );
+						if (val['domain'] == ClassId ) {
+							items = pushIfNotInArray(val, items);
+						}
 					}
 					
 				}
+			});
+			return items ;
+		}
+		function pushIfNotInArray(item, items) {
+
+			if ($.inArray(item, items) < 0) {
+				items.push(item) ;
 				
-			  });
-			//$(thisForm).append('<ul></ul>').append(items) ;
+			}
+			return items ;
 			
-			return items;
-				}
-		
-		
+		}
+
 		
 		return this ;
     }
