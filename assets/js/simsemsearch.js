fc043b11d0cabe5c9d6b6cdd7ad1ad20746f5c48
antoine37120 <ant.dupre@gmail.com>
2019-03-25 12:46:52 +0100

    #79 si valeur déja selectionnée, pas d'ajout

--- assets/js/simsemsearch.js
+++ assets/js/simsemsearch.js
@@ -1192,6 +1192,13 @@
 			if (temp_value == null ) {
 				return false ;
 			}
+			if (this.value_selected.length > 0) {
+
+				
+				if (Object.onArray(this.value_selected, temp_value) == true) {
+					return false ;
+				}
+			}
 			
 			this.value_selected.push(this.InputTypeComponent.GetValue()) ;
 			this.LabelValueSelected = this.InputTypeComponent.GetValueLabel() ;
@@ -2040,6 +2047,48 @@
 
 	return this ;
 }
+
+Object.onArray = function (arrayTosearch, objectTocompare) {
+	//console.log(arrayTosearch) ;
+	//console.log(objectTocompare) ;
+	var objectTocompare = objectTocompare ;
+	var temp_return = false ;
+	$.each( arrayTosearch, function( key, val ) {
+		
+		if (Object.compare(val, objectTocompare)) {
+			temp_return = true;
+		}
+	}) ;
+	return temp_return ;
+} ;
+
+Object.compare = function (obj1, obj2) {
+	//Loop through properties in object 1
+	for (var p in obj1) {
+		//Check property exists on both objects
+		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
+ 
+		switch (typeof (obj1[p])) {
+			//Deep compare objects
+			case 'object':
+				if (!Object.compare(obj1[p], obj2[p])) return false;
+				break;
+			//Compare function code
+			case 'function':
+				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
+				break;
+			//Compare values
+			default:
+				if (obj1[p] != obj2[p]) return false;
+		}
+	}
+ 
+	//Check object 2 for any extra properties
+	for (var p in obj2) {
+		if (typeof (obj1[p]) == 'undefined') return false;
+	}
+	return true;
+};
 
 	
  
