fa281d28ef526f88ac0f189f18722b889ac6b360
Thomas Francart <thomas.francart@sparna.fr>
2019-07-24 17:09:26 +0200

    Issue #102 : delete compile.sh

--- compile.sh
+++
@@ -1,3 +0,0 @@
-uglifyjs assets/js/sparnatural.js assets/js/EasyAutocomplete/jquery.easy-autocomplete.js assets/js/jquery-nice-select/js/jquery.nice-select.js assets/js/sparql-js/sparqljs-browser.js --output assets/js/sparnatural-min.js --source-map
-sass --style compressed assets/scss/sparnatural.scss assets/css/sparnatural-min.css
-sass assets/scss/sparnatural.scss assets/css/sparnatural.css
