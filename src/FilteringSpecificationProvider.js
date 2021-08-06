d9111f2fb1a43c4061d9015c30bc3d776ac875e2
Thomas Francart <thomas.francart@sparna.fr>
2021-08-06 16:22:28 +0200

    Real filtering based on classes and properties count

--- src/FilteringSpecificationProvider.js
+++ src/FilteringSpecificationProvider.js
@@ -10,39 +10,70 @@ export class FilteringSpecificationProvider {
 
 
 	notifyClassCount(classUri, count) {
-		console.log("Notifed of class count : "+classUri+" = "+count);
+		console.log("Notified of class count : "+classUri+" = "+count);
 		this.classesCount[classUri] = count;
 	}
 
 	notifyPropertyCount(domain, property, range, count) {
-		var key = {
-			'domain':domain,
-			'property':property,
-			'range':range
-		};
-		console.log("Notifed of properties count : "+key+" = "+count);
+		var key = "domain="+domain+"|"+"property="+property+"|"+"range="+range;
+		console.log("Notified of properties count : "+key+" = "+count);
 		this.propertiesCount[key] = count;
-	}
-
+	}	
 
 	getClassesInDomainOfAnyProperty() {
 		var items = this.delegateSpecificationProvider.getClassesInDomainOfAnyProperty();
-		return items;
+		return items.filter((classUri) => {
+			return(
+				this._isClassAvailable(classUri)
+				&&
+				this.getConnectedClasses(classUri).length > 0
+			);
+		});
 	}
 
-	getConnectedClasses(classId) {
-		var items = this.delegateSpecificationProvider.getConnectedClasses(classId);
-		return items;
+	getConnectedClasses(domainClassUri) {
+		var items = this.delegateSpecificationProvider.getConnectedClasses(domainClassUri);
+		return items.filter((rangeClassUri) => {
+			return(
+				this._isClassAvailable(rangeClassUri)
+				&&
+				this.getConnectingProperties(domainClassUri, rangeClassUri).length > 0
+			);
+		});
+	}
+
+	getConnectingProperties(domainClassId, rangeClassId) {
+		var items = this.delegateSpecificationProvider.getConnectingProperties(domainClassId, rangeClassId);
+		return items.filter((propertyUri) => {
+			return this._isPropertyAvailable(domainClassId, propertyUri, rangeClassId);
+		});
 	}
 
 	hasConnectedClasses(classId) {
 		return ( this.getConnectedClasses(classId).length > 0 );
 	}
 
-	getConnectingProperties(domainClassId, rangeClassId) {
-		var items = this.delegateSpecificationProvider.getConnectingProperties(domainClassId, rangeClassId);
-		return items;
+	_isClassAvailable(classUri) {
+		return (
+			// we don't have statistics for this class
+			typeof this.classesCount[classUri] == 'undefined'
+			||
+			// of we have a count > 0
+			(this.classesCount[classUri] > 0)
+		);
 	}
+
+	_isPropertyAvailable(domainClassId, propertyId, rangeClassId) {
+		var key = "domain="+domainClassId+"|"+"property="+propertyId+"|"+"range="+rangeClassId;
+		return (
+			// we don't have statistics for this class
+			typeof this.propertiesCount[key] == 'undefined'
+			||
+			this.propertiesCount[key] > 0
+		);
+	}
+
+
 
 	/** All these methods simply defer to delegate specification provider **/
 
