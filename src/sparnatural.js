0faf573ba084a5af7d6d02f1a97a06ca98dee564
Thomas Francart <thomas.francart@sparna.fr>
2022-04-26 18:11:08 +0200

    Issue #295 : remove unused Object functions

--- src/sparnatural.js
+++ src/sparnatural.js
@@ -2106,47 +2106,6 @@ UiuxConfig = require("./UiuxConfig.js");
 
 	return this ;
 } // end of Sparnatural function
-
-Object.onArray = function (arrayTosearch, objectTocompare) {
-	var objectTocompare = objectTocompare ;
-	var temp_return = false ;
-	$.each( arrayTosearch, function( key, val ) {
-		
-		if (Object.compare(val, objectTocompare)) {
-			temp_return = true;
-		}
-	}) ;
-	return temp_return ;
-} ;
-
-Object.compare = function (obj1, obj2) {
-	//Loop through properties in object 1
-	for (var p in obj1) {
-		//Check property exists on both objects
-		if (obj1.hasOwnProperty(p) !== obj2.hasOwnProperty(p)) return false;
- 
-		switch (typeof (obj1[p])) {
-			//Deep compare objects
-			case 'object':
-				if (!Object.compare(obj1[p], obj2[p])) return false;
-				break;
-			//Compare function code
-			case 'function':
-				if (typeof (obj2[p]) == 'undefined' || (p != 'compare' && obj1[p].toString() != obj2[p].toString())) return false;
-				break;
-			//Compare values
-			default:
-				if (obj1[p] != obj2[p]) return false;
-		}
-	}
- 
-	//Check object 2 for any extra properties
-	for (var p in obj2) {
-		if (typeof (obj1[p]) == 'undefined') return false;
-	}
-	return true;
-};
-
 	
  
 }( jQuery ));
