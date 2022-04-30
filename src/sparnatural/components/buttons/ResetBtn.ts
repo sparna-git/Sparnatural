b981b625facde9daeec058a7ef2c25ae7b94d512
SteinerPascal <st_pascal@outlook.com>
2022-04-30 15:53:42 +0200

    add Handler to resetbtn

---
+++ src/sparnatural/components/buttons/ResetBtn.ts
@@ -0,0 +1,14 @@
+import UiuxConfig from "../../../configs/fixed-configs/UiuxConfig";
+import HTMLComponent from "../HtmlComponent";
+
+class ResetBtn extends HTMLComponent {
+    constructor(ParentComponent:HTMLComponent,callBack:()=>void){
+        let widgetHtml = $('<p class="reset-form"><a>' +
+        UiuxConfig.ICON_RESET +
+        "</a></p>")
+        super("reset-wrapper",ParentComponent,widgetHtml)
+        this.html.on('click',(e:JQuery.ClickEvent)=>{
+            callBack()
+        })
+    }
+}
