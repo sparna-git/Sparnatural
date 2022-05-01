9692242688ebfe39dea2962d303be1e151b37515
SteinerPascal <st_pascal@outlook.com>
2022-05-01 21:34:25 +0200

    add NoOrderBtn

---
+++ src/sparnatural/components/buttons/NoOrderBtn.ts
@@ -0,0 +1,23 @@
+import UiuxConfig from "../../../configs/fixed-configs/UiuxConfig";
+import HTMLComponent from "../../HtmlComponent";
+
+
+class NoOrderBtn extends HTMLComponent{
+    selected:boolean = false
+    constructor(ParentComponent:HTMLComponent, callBack:()=>void){
+        let widgetHtml = $( UiuxConfig.ICON_NO_ORDER );
+        super("none",ParentComponent,widgetHtml)
+        // add clicklistener
+        this.widgetHtml.on('click',(e:JQuery.ClickEvent)=>{
+            this.selected = this.selected ? false:true
+            this.selected ? this.html.addClass('selected') : this.html.removeClass('selected')
+            callBack()
+        })
+    }
+
+    render(): this {
+        super.render()
+        return this
+    }    
+}
+export default NoOrderBtn
