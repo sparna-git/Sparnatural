672192d173d15ba4becf8120ebb90e22f177c4ce
SteinerPascal <st_pascal@outlook.com>
2022-05-02 22:33:09 +0200

    add ShowHideBtn

---
+++ src/sparnatural/components/buttons/ShowHideBtn.ts
@@ -0,0 +1,26 @@
+import UiuxConfig from "../../../configs/fixed-configs/UiuxConfig";
+import HTMLComponent from "../../HtmlComponent";
+
+
+class ShowHideBtn extends HTMLComponent{
+    selected = false;
+    constructor(ParentComponent:HTMLComponent, callBack:(displayed:boolean)=>void){
+        let widgetHtml = $(`<a class="displayButton">
+        ${UiuxConfig.ICON_ARROW_TOP} 
+        ${UiuxConfig.ICON_ARROW_BOTTOM}
+        </a>`);
+        super("VariableSelectorDisplay",ParentComponent,widgetHtml)
+        // add clicklistener
+        this.widgetHtml.on('click',(e:JQuery.ClickEvent)=>{
+            this.selected = this.selected ? false:true
+            this.selected ? this.html.addClass('displayed') : this.html.removeClass('displayed')
+            callBack(this.selected)
+        })
+    }
+
+    render(): this {
+        super.render()
+        return this
+    }    
+}
+export default ShowHideBtn
