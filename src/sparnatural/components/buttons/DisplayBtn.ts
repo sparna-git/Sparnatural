756a6c8912fb62d2b042158674969b489f4b8f18
SteinerPascal <st_pascal@outlook.com>
2022-06-09 20:04:12 +0200

    fix arrowchange when displaybtn click

--- src/sparnatural/components/buttons/DisplayBtn.ts
+++ src/sparnatural/components/buttons/DisplayBtn.ts
@@ -3,25 +3,40 @@ import HTMLComponent from "../HtmlComponent";
 
 class DisplayBtn extends HTMLComponent {
   selected = false;
+  callBack: (displayed: boolean) => void;
   constructor(
     ParentComponent: HTMLComponent,
     callBack: (displayed: boolean) => void
   ) {
     let widgetHtml = $(`<a class="displayButton">
-        ${UiuxConfig.ICON_ARROW_TOP} 
         ${UiuxConfig.ICON_ARROW_BOTTOM}
         </a>`);
     super("VariableSelectorDisplay", ParentComponent, widgetHtml);
-    // add clicklistener
-    this.widgetHtml.on("click", (e: JQuery.ClickEvent) => {
-      this.selected = this.selected ? false : true;
-      callBack(this.selected);
-    });
+    this.callBack = callBack
   }
 
   render(): this {
     super.render();
+    this.#addClickListener()
     return this;
+  }
+
+  #addClickListener(){
+    // add clicklistener
+    this.widgetHtml.on("click", (e: JQuery.ClickEvent) => {
+      this.selected = this.selected ? false : true;
+      if(this.selected){
+        this.widgetHtml = $(`<a class="displayButton">
+        ${UiuxConfig.ICON_ARROW_TOP}
+        </a>`);
+      } else{
+        this.widgetHtml = $(`<a class="displayButton">
+        ${UiuxConfig.ICON_ARROW_BOTTOM}
+        </a>`);
+      }
+      this.callBack(this.selected);
+      this.render()
+    });
   }
 }
 export default DisplayBtn;
