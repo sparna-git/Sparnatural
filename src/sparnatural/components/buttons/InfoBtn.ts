bec3ce540921cacdebd72f607454cbbc1ea4fd16
SteinerPascal <st_pascal@outlook.com>
2022-06-21 21:45:46 +0200

    fix add tippysettings to infoBtn

--- src/sparnatural/components/buttons/InfoBtn.ts
+++ src/sparnatural/components/buttons/InfoBtn.ts
@@ -4,14 +4,16 @@ import TippyInfo from "./TippyInfo";
 
 class InfoBtn extends HTMLComponent{
     infoMessage:string
-    constructor(parentComponent:HTMLComponent,infoMessage:string){
+    tippySettings: any;
+    constructor(parentComponent:HTMLComponent,infoMessage:string,tippySettings?:any){
         let widgetHtml = $(`${UiuxConfig.ICON_CIRCLE_INFO}`)
         super('circle-info',parentComponent,widgetHtml)
         this.infoMessage = infoMessage
+        this.tippySettings = tippySettings
     }
     render(): this {
         super.render()
-        new TippyInfo(this,this.infoMessage)
+        new TippyInfo(this,this.infoMessage,this.tippySettings)
         return this
     }
 }
