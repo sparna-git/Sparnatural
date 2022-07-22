bb585d607217b4d81456f79ded072f06fc616599
antoine37120 <ant.dupre@gmail.com>
2022-07-22 15:20:01 +0200

    #21 add regular icon and fix unselect button

--- src/sparnatural.ts
+++ src/sparnatural.ts
@@ -11,7 +11,9 @@ import $ from "jquery";
 require("@fortawesome/fontawesome-free");
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
 library.add(fas);
+library.add(far);
 
 import { getSettings, mergeSettings } from "./configs/client-configs/settings";
 import Sparnatural from "./sparnatural/components/Sparnatural";
