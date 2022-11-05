c68920f3a0006ed323d19183268f312c652d2195
FRANCART Thomas <thomas.francart@ext.europarl.europa.eu>
2022-11-05 18:45:51 +0100

    Fix color of option arrow

--- src/sparnatural/components/SparnaturalComponent.ts
+++ src/sparnatural/components/SparnaturalComponent.ts
@@ -20,6 +20,7 @@ class Sparnatural extends HTMLComponent {
   BgWrapper: BgWrapper;
   SubmitSection: SubmitSection;
   variableSection: VariableSection;
+  // filter that is applied to optional/not exists green arrows, based on its ID
   filter = $(
     '<svg data-name="Calque 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 0 0" style="width:0;height:0;display:block"><defs><filter style="color-interpolation-filters:sRGB;" inkscape:label="Drop Shadow" id="filter19278" x="-0.15483875" y="-0.11428573" width="1.3096775" height="1.2714286"><feFlood flood-opacity="0.811765" flood-color="rgb(120,120,120)" result="flood" id="feFlood19268" /><feComposite in="flood" in2="SourceGraphic" operator="out" result="composite1" id="feComposite19270" /><feGaussianBlur in="composite1" stdDeviation="2" result="blur" id="feGaussianBlur19272" /><feOffset dx="3.60822e-16" dy="1.8" result="offset" id="feOffset19274" /><feComposite in="offset" in2="SourceGraphic" operator="atop" result="composite2" id="feComposite19276" /></filter></defs></svg>'
   );
@@ -46,11 +47,12 @@ class Sparnatural extends HTMLComponent {
         that,
         that.specProvider
       ).render();
-      that.html.append(that.filter);
+      
       //BGWrapper must be rendered first
       that.html[0].dispatchEvent(
         new CustomEvent("initGeneralEvent", { bubbles: true })
-      );      
+      );
+      that.html.append(that.filter);      
     });
     
     return this;
