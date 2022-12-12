e2a3154189ff5a5749ec8d740948f26b70ecbebc
Thomas Francart <thomas.francart@sparna.fr>
2022-12-12 23:09:52 +0100

    Update script to generate ontology doc

---
+++ ontologies/generate.sh
@@ -0,0 +1,12 @@
+java -jar java-11-widoco-1.4.17-jar-with-dependencies.jar \
+-ontFile sparnatural-config-core.owl \
+-outFolder sparnatural-config-core \
+-confFile sparnatural-config-core.properties \
+-rewriteAll \
+-lang en-fr \
+-htaccess \
+-rewriteBase /ontologies/sparnatural-config-core \
+-includeAnnotationProperties \
+-excludeIntroduction \
+-uniteSections \
+-noPlaceHolderText
