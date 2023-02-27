b83678d360bda20980c5b52b98c8a9f61279a79c
SteinerPascal <st_pascal@outlook.com>
2023-02-27 17:14:13 +0100

    types for  #448 and #187

--- src/sparnatural/settings/ISettings.ts
+++ src/sparnatural/settings/ISettings.ts
@@ -15,6 +15,16 @@ interface ISettings {
   sparqlPrefixes?: { [key: string]: string };
   defaultEndpoint?: string;
   localCacheDataTtl?: number;
+  dataEndpoints:Array<{
+    endpoint:string;
+    credentials: "omit" | "same-origin" | "include";
+    cache: "default" | "reload" | "no-cache";
+    mode: "cors" | "no-cors" | 'same-origin' | 'navigate';
+    method: "GET" | "POST"
+    headers:{
+      [key: string]: string;
+    }
+  }>;
   // TODO : this should not be exposed.
   // Only the language parametre is exposed, but the actual labels content should not
   langSearch?: any;
