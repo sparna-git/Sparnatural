4905e829dc2c307c7ea47633fb88712b0101d468
Thomas Francart <thomas.francart@sparna.fr>
2023-06-18 18:02:07 +0200

    Fix #516

--- src/sparnatural/components/builder-section/groupwrapper/groupwrapperevents/events/RemoveEndClass.ts
+++ src/sparnatural/components/builder-section/groupwrapper/groupwrapperevents/events/RemoveEndClass.ts
@@ -8,6 +8,11 @@ export function removeEndClass(grpWrapper: GroupWrapper) {
   if (grpWrapper.whereChild)
     grpWrapper.whereChild.html[0].dispatchEvent(new CustomEvent("onRemoveGrp"));
 
+  // notify it is incomplete - it will become higher and remove the "completed" class
+  grpWrapper.CriteriaGroup.EndClassGroup.html[0].dispatchEvent(
+    new CustomEvent("onGrpInputNotCompleted", { bubbles: true })
+  );
+
   let startVal = grpWrapper.CriteriaGroup.StartClassGroup.startClassVal;
   grpWrapper.CriteriaGroup.html.empty();
   grpWrapper.CriteriaGroup.html.remove();
@@ -21,12 +26,13 @@ export function removeEndClass(grpWrapper: GroupWrapper) {
   grpWrapper.optionState = OptionTypes.NONE
   // set StartClassVal back to its original value
   grpWrapper.CriteriaGroup.StartClassGroup.startClassVal = startVal;
-  let inputTypeComponent =
-    grpWrapper.CriteriaGroup.StartClassGroup.inputSelector;
+  let inputTypeComponent = grpWrapper.CriteriaGroup.StartClassGroup.inputSelector;
   inputTypeComponent.oldWidget.val(startVal.type).niceSelect("update");
   // nice-select is 2nd place in childrenslist. move away from nice-select...
   inputTypeComponent.html[0].children[1].classList.add("disabled");
 
+
+  
   // decrease the SPARQL var counter by 2 : one for the previous EndClassGroup, one for the new StartClassGroup
   // that gets its original value back
   grpWrapper.html[0].dispatchEvent(
