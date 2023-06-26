8ccdfb037f8fb5b04d674df5aa3c740ed3eb1796
Thomas Francart <thomas.francart@sparna.fr>
2023-06-26 11:28:24 +0200

    #457 - need to set the message, and maybe add a title attribute ?

--- src/sparnatural/components/buttons/ResetBtn.ts
+++ src/sparnatural/components/buttons/ResetBtn.ts
@@ -1,18 +1,32 @@
 import UiuxConfig from "../IconsConstants";
 import HTMLComponent from "../HtmlComponent";
+import { TOOLTIP_CONFIG } from "../../settings/defaultSettings";
+import TippyInfo from "./TippyInfo";
 
 class ResetBtn extends HTMLComponent {
+  tippySettings: any;
+
   constructor(ParentComponent: HTMLComponent, callBack: () => void) {
     let widgetHtml = $(
       '<p class="reset-form"><a>' + UiuxConfig.ICON_RESET + "</a></p>"
     );
+
     super("reset-wrapper", ParentComponent, widgetHtml);
     this.widgetHtml.on("click", (e: JQuery.ClickEvent) => {
       callBack();
     });
+
+    // set a tooltip on the reset button
+    /*
+    this.tippySettings = Object.assign({}, TOOLTIP_CONFIG);
+    this.tippySettings.placement = 'right-start';
+    this.tippySettings.offset = [-15, -54];
+    this.tippySettings.delay = [300, 0];
+    */
   }
   render(): this {
     super.render();
+    // new TippyInfo(this, "blah blah", this.tippySettings);
     return this;
   }
 }
