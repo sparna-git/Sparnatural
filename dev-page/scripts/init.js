be8cbc46e4dcf846fb412f007a61420818e86e8b
Thomas Francart <thomas.francart@sparna.fr>
2024-04-05 16:24:37 +0200

    Update dev-page

--- dev-page/scripts/init.js
+++ dev-page/scripts/init.js
@@ -2,19 +2,14 @@
   This file shows how to integrate SparNatural into your website. 
 */
 
-export const sparnatural = document.querySelector("spar-natural");
-import {yasqe} from './initYasgui.js'
-
+const sparnatural = document.querySelector("spar-natural");
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const lang = urlParams.get('lang')
 
 
-
-	
-
-
+console.log("init sparnatural...");
 sparnatural.addEventListener("init", (event) => {  
 	sparnatural.configuration = {
 	  headers: { "User-Agent" : "This is Sparnatural calling" },
@@ -36,6 +31,35 @@ sparnatural.addEventListener("submit", (event) => {
   // trigger the query from YasQE
   yasqe.query();
 });
+
+console.log("init yasr & yasqe...");
+const yasqe = new Yasqe(document.getElementById("yasqe"), {
+    requestConfig: { endpoint: $('#endpoint').text() },
+    copyEndpointOnNewTab: false  
+});
+
+
+const yasr = new Yasr(document.getElementById("yasr"), {
+    //this way, the URLs in the results are prettified using the defined prefixes in the query
+    getUsedPrefixes : yasqe.getPrefixesFromQuery,
+    "drawOutputSelector": false,
+    "drawDownloadIcon": false,
+    // avoid persistency side-effects
+    "persistency": { "prefix": false, "results": { "key": false }}
+});
+
+// link yasqe and yasr
+yasqe.on("queryResponse", function(_yasqe, response, duration) {
+    yasr.setResponse(response, duration);
+    sparnatural.enablePlayBtn() ;
+}); 
+
+
+
+
+
+
+
 
 document.getElementById('switch-language').onclick = function() {
   document.querySelector("spar-natural").setAttribute("lang", "en");
